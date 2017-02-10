52e9eab8521e0d900d95c3ace1c5f2af31293636
Siolan <reysimon@orange.fr>
2017-02-10 18:05:27 +0100

    Update game.scala

--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -39,16 +39,16 @@ object Ksparov {
   def init_board(){
     for (p <- 0 to 1) {
       for(i <- 1 to 8) {
-        board((1-p)*16 + (i-1)) = new Pawn("pawn",p+1,1+p*5,i-1)
+        board((1-p)*16 + (i-1)) = new Pawn(p+1,1+p*5,i-1)
         }
-      board( 8 + (1-p)*16) = new Rook("rook",p+1,1+p*7,1)
-      board( 9 + (1-p)*16) = new Rook("rook",p+1,1+p*7,8)
-      board( 10 + (1-p)*16) = new Knight("knight",p+1,1+p*7,2)
-      board( 11 + (1-p)*16) = new Knight("knight",p+1,1+p*7,7)
-      board( 12 + (1-p)*16) = new Bishop("bishop",p+1,1+p*7,3)
-      board( 13 + (1-p)*16) = new Bishop("bishop",p+1,1+p*7,6)
-      board( 14 + (1-p)*16) = new King("king",p+1,1+p*7,4+(1-p))
-      board( 15 + (1-p)*16) = new Queen("queen",p+1,1+p*7,5 - (1-p))
+      board( 8 + (1-p)*16) = new Rook(p+1,1+p*7,1)
+      board( 9 + (1-p)*16) = new Rook(p+1,1+p*7,8)
+      board( 10 + (1-p)*16) = new Knight(p+1,1+p*7,2)
+      board( 11 + (1-p)*16) = new Knight(p+1,1+p*7,7)
+      board( 12 + (1-p)*16) = new Bishop(p+1,1+p*7,3)
+      board( 13 + (1-p)*16) = new Bishop(p+1,1+p*7,6)
+      board( 14 + (1-p)*16) = new King(p+1,1+p*7,4+(1-p))
+      board( 15 + (1-p)*16) = new Queen(p+1,1+p*7,5 - (1-p))
     }
   }
 
