441cb170ef6878a89829787724a90aed9f4a19ba
PerrinJeremie <jperrin.pjeremie@gmail.com>
2017-02-10 16:18:42 +0100

    to pull

--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -5,8 +5,13 @@ import java.awt.Color
 import scala.swing.BorderPanel.Position._
 
 abstract class Player {
-  var id:Int = 0
-  def getmove : (Int,Int)
+  val id:Int = 0
+  def getmove : Unit
+}
+
+class Human(n:Int) extends Player {
+  override val id = n
+  def getmove = ()
 }
 
 object Constants {
@@ -31,23 +36,32 @@ object Ksparov {
   }
 
   var board : Array[Piece] = new Array[Piece](32)
-
   def init_board(){
     for (p <- 0 to 1) {
       for(i <- 1 to 8) {
-        board((1-p)*16 + (i-1)) = new Pawn("pawn",p,2+(1-p)*5,i)
+        board((1-p)*16 + (i-1)) = new Pawn("pawn",p,2+p*5,i)
         }
-      board( 8 + (1-p)*16) = new Rook("rook",p,(1-p)*8,1)
-      board( 9 + (1-p)*16) = new Rook("rook",p,(1-p)*8,8)
-      board( 10 + (1-p)*16) = new Knight("knight",p,(1-p)*8,2)
-      board( 11 + (1-p)*16) = new Knight("knight",p,(1-p)*8,7)
-      board( 12 + (1-p)*16) = new Bishop("bishop",p,(1-p)*8,3)
-      board( 13 + (1-p)*16) = new Bishop("bishop",p,(1-p)*8,6)
-      board( 14 + (1-p)*16) = new Queen("queen",p,(1-p)*8,4+(1-p))
-      board( 15 + (1-p)*16) = new Rook("king",p,(1-p)*8,5 - (1-p))
+      board( 8 + (1-p)*16) = new Rook("rook",p,1+p*7,1)
+      board( 9 + (1-p)*16) = new Rook("rook",p,1+p*7,8)
+      board( 10 + (1-p)*16) = new Knight("knight",p,1+p*7,2)
+      board( 11 + (1-p)*16) = new Knight("knight",p,1+p*7,7)
+      board( 12 + (1-p)*16) = new Bishop("bishop",p,1+p*7,3)
+      board( 13 + (1-p)*16) = new Bishop("bishop",p,1+p*7,6)
+      board( 14 + (1-p)*16) = new King("king",p,1+p*7,4+(1-p))
+      board( 15 + (1-p)*16) = new Queen("queen",p,1+p*7,5 - (1-p))
     }
 
   }
+
+  def init_game(n : Int){
+    Ksparov.init_board()
+    n match {
+      case 1 => Ksparov.init_board()
+
+      case _ => ()
+    }
+  }
+
   /*On ne fait qu'une application, c'est simplement que l'on change de 
   frame pour changer de vue. On ne touche donc pas à Application mais que à frame !*/
   var application = new SimpleSwingApplication {
@@ -57,9 +71,7 @@ object Ksparov {
   def main(argv : Array[String]) {
     application.main(Array())
     Ksparov.init_board()
-    for (i<-0 to Ksparov.board.length - 1){
-      println((Ksparov.board(i).pos_x - 1) * 8 + (Ksparov.board(i).pos_y - 1))
-    }
-    /*DrawActions.draw_game_board(Ksparov.board)*/
+    DrawActions.draw_game_board(Ksparov.board)
+
   }
 }
---
+++ src/main/scala/solve.scala
@@ -0,0 +1,7 @@
+/*class AI(player : Int) extends Player{
+  var id = player
+  def getmove(){
+    val r = scala.util.random
+    Ksparov.board((1-id)*16 + r.nextInt(16)).move(0,0,Ksparov.board)
+  }
+}*/
