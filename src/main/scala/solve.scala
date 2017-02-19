27e250ae624cc72b2d1e8b61fb3b7abcd402bbb5
Siolan <reysimon@orange.fr>
2017-02-19 17:23:57 +0100

    Amélioration de l'IA + Rectification des numéros de bord de plateau

--- src/main/scala/solve.scala
+++ src/main/scala/solve.scala
@@ -1,18 +1,23 @@
 class AI(player : Int) extends Player(player){
 	ai = true
   override def getmove {
+    var already_check = new Array[Boolean](16)
+    for(i <- 0 to 15) {
+      already_check (i) = false
+    }
     val r = scala.util.Random
     var b = true
-    var i = 0
-    while (b || i < 1000) {
-      var ind = (1-id)*16 + r.nextInt(16)
-      var t = Ksparov.board(ind).possible_moves(Ksparov.board)
-      if (t.nonEmpty) {
-        b = false
-        var (i,j) = t(0)
-        Ksparov.board(ind).move(i,j,Ksparov.board)
+    while (b) {
+      var ind = r.nextInt (16)
+      if (!already_check (ind)) {
+        already_check (ind) = true
+        var t = Ksparov.board ((1 - id) * 16 + ind).possible_moves (Ksparov.board)
+        if (t.nonEmpty) {
+          b = false
+          var (i,j) = t(0)
+          Ksparov.board((1 - id) * 16 + ind).move(i,j,Ksparov.board)
+        }
       }
-      i += 1
     }
     DrawActions.draw_game_board(Ksparov.board)
     Constants.players(Constants.curr_player).moved = true
