57bf16fb1a6da88325c747cead89bbcba338a0bf
Siolan <reysimon@orange.fr>
2017-02-19 16:42:23 +0100

    Compteur pour l'IA

--- src/main/scala/solve.scala
+++ src/main/scala/solve.scala
@@ -3,7 +3,8 @@ class AI(player : Int) extends Player(player){
   override def getmove {
     val r = scala.util.Random
     var b = true
-    while (b) {
+    var i = 0
+    while (b || i < 1000) {
       var ind = (1-id)*16 + r.nextInt(16)
       var t = Ksparov.board(ind).possible_moves(Ksparov.board)
       if (t.nonEmpty) {
@@ -11,6 +12,7 @@ class AI(player : Int) extends Player(player){
         var (i,j) = t(0)
         Ksparov.board(ind).move(i,j,Ksparov.board)
       }
+      i += 1
     }
     DrawActions.draw_game_board(Ksparov.board)
     Constants.players(Constants.curr_player).moved = true
