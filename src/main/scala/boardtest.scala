3d9dd94ecb1f3d8b955a030f8fa5e2e4f4adcfe3
Siolan <reysimon@orange.fr>
2017-05-02 13:52:48 +0200

    Blabla2

--- src/main/scala/boardtest.scala
+++ src/main/scala/boardtest.scala
@@ -19,6 +19,21 @@ def init_alt_board (s : String) = {
         Ksparov.curr_game.board(14 + (1 - p) * 16) = Ksparov.curr_game.kings(p)
         Ksparov.curr_game.board(15 + (1 - p) * 16) = new Queen(p, -1, -1, 0)
       }
+    case "roque" => 
+      for (p <- 0 to 1) {
+        for(i <- 0 to 7) {
+          Ksparov.curr_game.board((1 - p) * 16 + i) = new Pawn(p, -1, -1, 0)
+        }
+        Ksparov.curr_game.board(8 + (1 - p) * 16) = new Rook(p, 0, (1 - p) * 7, 0)
+        Ksparov.curr_game.board(9 + (1 - p) * 16) = new Rook(p, 7, (1 - p) * 7, 0)
+        Ksparov.curr_game.board(10 + (1 - p) * 16) = new Knight(p, 1, (1 - p) * 7, 0)
+        Ksparov.curr_game.board(11 + (1 - p) * 16) = new Knight(p, 6, (1 - p) * 7, 0)
+        Ksparov.curr_game.board(12 + (1 - p) * 16) = new Bishop(p, 2, (1 - p) * 7, 0)
+        Ksparov.curr_game.board(13 + (1 - p) * 16) = new Bishop(p, 5, (1 - p) * 7, 0)
+        // Kings are instantiate in a Game to have a quick access to them 
+        Ksparov.curr_game.board(14 + (1 - p) * 16) = Ksparov.curr_game.kings(p)
+        Ksparov.curr_game.board(15 + (1 - p) * 16) = new Queen(p, 3, (1 - p) * 7, 0)
+      }
     case "legal" => 
       for (p <- 0 to 1) {
         for(i <- 0 to 7) {
